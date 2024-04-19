53a72842ad501aed1a9f80931d6d895fb6790106
gancsov <71602533755@szily.hu>
2024-04-19 13:24:48 +0200

    email gener

--- feladat2/masodik.js
+++ feladat2/masodik.js
@@ -1,6 +1,13 @@
 function email() {
-    // var nev = document.getElementById("nev").innerHTML
-  }
-
-  var nev = document.getElementById("nev").textContent
+  var nev = document.getElementById("nev").value;
+  var ev = document.getElementById("ev").value;
   console.log(nev)
+  const tomb = nev.split(" ")
+  var nev1 = tomb[0].substring(0,3) 
+  var nev2 = tomb[1].substring(0,3) 
+  var ev0 = ev.substring(3,4)
+  var email = nev1+nev2+ev0+"@duck.com"
+  var kiir = document.getElementById("generalt")
+  kiir.textContent=email
+  console.log(email)
+  }
